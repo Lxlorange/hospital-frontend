87abd6672348132c594e85972a3cd326b9a32295
liruize830 <23301069@bjtu.edu.cn>
2025-10-12 10:43:59 +0800

    添加医生数据模块

---
+++ src/api/doctor/DoctorModel.ts
@@ -0,0 +1,17 @@
+// 医生数据类型
+export type DoctorType = {
+    username:string,
+    password:string,
+    phone:string,
+    email:string,
+    sex:string,
+    Department:string,
+    education:string,
+    job_title:string, 
+    image:string,
+    introduction:string,
+    orderNum:string,
+    visit_address: string,
+    good_at: string,
+    price: string
+} 
