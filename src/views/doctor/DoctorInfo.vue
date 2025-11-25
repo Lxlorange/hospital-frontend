7bbedb0af00b4eb88b38b6f596e313e2800dc7f9
liruize830 <23301069@bjtu.edu.cn>
2025-11-25 10:41:26 +0800

    Update DoctorInfo.vue
    优化个人信息界面

--- src/views/doctor/DoctorInfo.vue
+++ src/views/doctor/DoctorInfo.vue
@@ -15,8 +15,8 @@
           </div>
         </div>
         <div class="doctor-edit">
-          <el-button type="primary" size="small" @click="editDoctorInfo">编辑资料</el-button>
-          <el-button type="primary" size="small" @click="requestLeave">申请请假</el-button>
+          <el-button type="warning" size="middle" @click="editDoctorInfo">编辑资料</el-button>
+          <el-button type="success" size="middle" @click="requestLeave">申请请假</el-button>
         </div>
       </div>
     </el-card>
